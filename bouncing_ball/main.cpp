4047ccfc804850ad4e4fe173dc860d1c2815eaa0
Peppo <pietrofacchini46@gmail.com>
2023-01-23 00:44:12 +0100

    Now you can render multiple balls at the same time

--- bouncing_ball/main.cpp
+++ bouncing_ball/main.cpp
@@ -2,8 +2,15 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <math.h>
+#include <thread>
+#include <chrono>
+#include <vector>
 
 using namespace std;
+using namespace chrono;
+
+#define W 1920	
+#define H 1080
 
 class vec2 {
 public:
@@ -33,42 +40,98 @@ int rrand(int min,int max) {
 	return min + (std::rand() % (max - min + 1));
 }
 
-int main() {
-	int seed = time(0);
-	srand(seed);
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY,"2");
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY,"2");
-	SDL_CreateWindowAndRenderer(1920,1080,SDL_WINDOW_BORDERLESS,&window,&renderer);
-	SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY,"2");
-	SDL_RenderSetVSync(renderer,1);
-	vec2 pos = {1920/2,1080/2};
-	vec2 size = {15,15};
-	double speed = 5;
-	double angle_o = 45;
-	cout << "seed: " << time(0) << endl;
-	//SDL_SetRenderDrawColor(renderer,25,25,25,255);
-	//SDL_RenderClear(renderer);
-	while(1) {
-		SDL_PollEvent(nullptr);
-		SDL_SetRenderDrawColor(renderer,25,25,25,255);
-		SDL_RenderClear(renderer);
-		if((pos.x + (size.x*2)) >= 1920 || (pos.x - (size.x * 2)) <= 0 || (pos.y + (size.y * 2)) >= 1080 || (pos.y - (size.y * 2)) <= 0) {
+
+double frames = 0;
+double elapsed = 0;
+int tAdded = 0;
+int elapsedSeconds = 0;
+bool endTs = false;
+bool inPause = false;
+
+void uptimer() {
+	while(!endTs) {
+		this_thread::sleep_for(chrono::milliseconds(950));
+		tAdded++;
+		elapsed += 0.1;
+		elapsedSeconds = (int)(tAdded / 10);
+	}
+}
+
+void fpsUpdater() {
+	while(!endTs) {
+		cout << "fps: " << frames/elapsed << "         \r";
+	}
+}
+
+class ball {
+public:
+	double xRand = rrand((W / 4),W - (W / 4));
+	double yRand = rrand((H / 4),H - (H / 4));
+	vec2 pos = {W / 2,H / 2};
+	vec2 size = {4,4};
+	double speed = 4;
+	double angle_o = rrand(0,360);
+	void update() {
+		if((pos.x + (size.x * 2)) >= W || (pos.x - (size.x * 2)) <= 0 || (pos.y + (size.y * 2)) >= H || (pos.y - (size.y * 2)) <= 0) {
 			//cout << "bounce" << endl;
-			angle_o += 90;
-			//angle_o += rrand(80,120);
+			//angle_o += 90;
+			angle_o += rrand(90,130);
 			//speed = rrand(1,10)/10.0;
 			//SDL_SetRenderDrawColor(renderer,255,255,255,255);
 			//SDL_RenderClear(renderer);
 		}
-		//SDL_SetRenderDrawColor(renderer,rrand(0,255),rrand(0,255),rrand(0,255),255);
-		SDL_SetRenderDrawColor(renderer,0xFF,0xFD,0xD0,195);
 		vec2 v = angle(angle_o);
-		pos.x+=v.x*speed;
-		pos.y+=v.y*speed;
-		drawCircle(renderer,pos,size.x,1);
-		SDL_RenderPresent(renderer);
-		cout << "x: " << pos.x << ", y: " << pos.y << ", angle: " << rads(angle_o) << "\r";
+		pos.x += v.x * speed;
+		pos.y += v.y * speed;
+		//cout << "x: " << pos.x << ", y: " << pos.y << ", angle: " << (angle_o) << "\r";
+	}
+};
+
+int main() {
+	int seed = time(0);
+	srand(seed);
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_CreateWindowAndRenderer(W,H,0,&window,&renderer);
+	SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
+	SDL_RenderSetVSync(renderer,1);
+	cout << "seed: " << time(0) << endl;
+	SDL_SetRenderDrawColor(renderer,25,25,25,255);
+	SDL_RenderClear(renderer);
+
+	vector<ball> balls(500); // balls to render at start
+
+	thread timT(uptimer);
+	thread fpsT(fpsUpdater);
+	SDL_Event e;
+	while(1) {
+		SDL_PollEvent(&e);
+		if(!inPause)
+		{
+			SDL_SetRenderDrawColor(renderer,25,25,25,255);
+			SDL_RenderClear(renderer);
+			SDL_SetRenderDrawColor(renderer,0xFF,0xFD,0xD0,195);
+			for(int i = 0; i < balls.size(); i++) {
+				balls[i].update();
+				drawCircle(renderer,balls[i].pos,balls[i].size.x,1);
+			}
+			/*if(tAdded % 1 == 0) {
+				for(int i = 0; i < 200; i++) {
+					balls.push_back(ball());
+				}
+			}*/
+			SDL_RenderPresent(renderer);
+			frames++;
+			if(e.type == SDL_QUIT || e.key.keysym.scancode == SDL_SCANCODE_Q) {
+				endTs = true;
+				timT.join();
+				fpsT.join();
+				return 0;
+			}
+		}
+		if(e.type == SDL_KEYDOWN) {
+			if(e.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+				inPause = !inPause;
+			}
+		}
 	};
 }
